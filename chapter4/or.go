810d9e9be4ac4245fc734ac77afc1941c703da0a
masa-suzu <szk.masato@icloud.com>
2018-11-09 22:47:32 +0900

    ch4p98

---
+++ chapter4/or.go
@@ -0,0 +1,52 @@
+package chapter4
+
+import (
+	"fmt"
+	"time"
+)
+
+func OR(channels ...<-chan interface{}) <-chan interface{} {
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-OR(append(channels[3:], orDone)...):
+			}
+		}
+	}()
+	return orDone
+}
+func ORSample() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+	start := time.Now()
+	<-OR(
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+		sig(1*time.Second),
+	)
+	fmt.Printf("done after %v", time.Since(start))
+}
--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "github.com/masa-suzu/concurrency-in-go/chapter4"
 func main() {
 	chapter4.Done1()
 	chapter4.Done2()
+	chapter4.ORSample()
 }
